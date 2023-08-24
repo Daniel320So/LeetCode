0f849e2c57372e71c9ebf32606474bdfb6b4ce04
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-08-24 11:05:47 -0400

    Time: 56 ms (74.16%) | Memory: 44.5 MB (33.70%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+    let result = null
+
+    while (head) {
+        result = new ListNode(head.val, result)
+        head = head.next
+    }
+
+    return result
+};
