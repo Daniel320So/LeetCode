0ab9db8e7fa2b8fb62bd7dcde1cf3f2c45f9382a
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-09-25 13:55:46 -0400

    Time: 67 ms (32.87%) | Memory: 47 MB (14.35%) - LeetSync

--- 389-find-the-difference/find-the-difference.ts
+++ 389-find-the-difference/find-the-difference.ts
@@ -2,14 +2,12 @@ function findTheDifference(s: string, t: string): string {
 
     let tObj = {};
     let sObj = {};
-    for ( let i =0; i < t.length; i++) {
-        tObj[t[i]] =  tObj[t[i]] !== undefined?  tObj[t[i]] + 1 : 1; 
-    }
-
+    let tLength = t.length - 1;
     for ( let i =0; i < s.length; i++) {
+        tObj[t[i]] =  tObj[t[i]] !== undefined?  tObj[t[i]] + 1 : 1; 
         sObj[s[i]] =  sObj[s[i]] !== undefined?  sObj[s[i]] + 1 : 1; 
     }
-
+    tObj[t[tLength]] =  tObj[t[tLength]] !== undefined?  tObj[t[tLength]] + 1 : 1;
     let keys = Object.keys(tObj);
     for ( let i =0; i< keys.length; i++) {
         if (!sObj[keys[i]] || tObj[keys[i]] !== sObj[keys[i]]) {
