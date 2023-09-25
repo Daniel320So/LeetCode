8156666897bea44dda373ae133f9bebae80be247
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-09-25 13:54:18 -0400

    Time: 70 ms (20.37%) | Memory: 46.4 MB (17.59%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.ts
@@ -0,0 +1,22 @@
+function findTheDifference(s: string, t: string): string {
+
+    let tObj = {};
+    let sObj = {};
+    for ( let i =0; i < t.length; i++) {
+        tObj[t[i]] =  tObj[t[i]] !== undefined?  tObj[t[i]] + 1 : 1; 
+    }
+
+    for ( let i =0; i < s.length; i++) {
+        sObj[s[i]] =  sObj[s[i]] !== undefined?  sObj[s[i]] + 1 : 1; 
+    }
+
+    let keys = Object.keys(tObj);
+    for ( let i =0; i< keys.length; i++) {
+        console.log(sObj[keys[i]])
+        if (!sObj[keys[i]] || tObj[keys[i]] !== sObj[keys[i]]) {
+            return keys[i]
+        }
+    }
+
+    return "";
+}
